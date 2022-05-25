2063a7322d75406a29c7407c6dbab955955517a5
kingdomofazeroth <smithbook009@gmail.com>
2022-05-25 18:24:25 +0300

    eng

---
+++ main.js
@@ -0,0 +1,28 @@
+
+const bodyTag = document.getElementsByTagName('body')[0];
+const refreshPage = document.querySelectorAll('.refresh-page');
+// const loadingPage = document.getElementsByClassName('loader spinner');
+const loadingPage = document.querySelector('.loader.spinner')
+
+
+refreshPage.forEach( item => {
+    item.addEventListener('click' , () => {
+        
+        setTimeout(() => {
+            window.location.reload();
+        },7000);
+
+
+         if(refreshPage) {   
+                bodyTag.style.opacity = "0.5";  
+                loadingPage.classList.remove('spinner')  
+         }
+
+   
+
+    })
+});
+
+
+
+
